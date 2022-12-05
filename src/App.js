f1b3ffea1964137ba44d7b9a2337de27a67d2f9d
Amol Thite <amol.thite@apptware.com>
2022-12-05 13:03:19 +0530

    initial commit and project structure

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Container from "./containers";
+const App = () => {
+  return (
+    <div style={{height: '100%'}}>
+      <Container/>
+    </div>
+  );
+}
+
+export default App;
