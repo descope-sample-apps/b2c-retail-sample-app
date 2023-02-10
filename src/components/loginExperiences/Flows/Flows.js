69ede4abc13aa9911d4e03aff941d60ddbebcfc6
Gilad Shriki <shriki@descope.com>
2023-02-09 18:27:20 -0800

    replace flow image with code

--- src/components/loginExperiences/Flows/Flows.js
+++ src/components/loginExperiences/Flows/Flows.js
@@ -1,10 +1,35 @@
-import { Button, Typography, Image} from "antd";
+import { Button, Typography} from "antd";
 import React from "react";
-import "./flows.css";
-import flow_image from "../../../assets/flow.jpg"
+import "../SDK/sdk.scss";
+import "prismjs/themes/prism-tomorrow.css";
+import { useEffect } from "react";
+import Prism from "prismjs";
+
 
 const Flows = () => {
-  
+    // The code snippet you want to highlight, as a string
+    const code = 
+`// Wrapping react App with <AuthProvider>
+<AuthProvider projectId=YOUR_PROJECT_ID>
+    <App />
+</AuthProvider>
+
+// Embedding Descope flow component
+<Descope
+    flowId="sign-up-or-in"
+    onSuccess={(e) => {
+        console.log("Logged in")
+        console.log("Username", e.detail.user.name)
+        console.log("Email", e.detail.user.email)
+    }}
+    onError={(e) => {
+        console.log("Error!", e)
+    }
+    theme="light"
+/>`;
+useEffect(() => {
+  Prism.highlightAll();
+}, []);
   return (
     <div className="main-flow-container">
       <div className="left-flow">
@@ -28,7 +53,16 @@ const Flows = () => {
       </div>
       <div className="right-flow">
         <div className="inner-div">
-        <Image src={flow_image} className="img-flow"/>
+          <div className="Code">
+            <pre style={{ margin: 0 }}>
+              <code
+                className={`language-javascript`}
+                style={{ "overflow-y": "scroll" }}
+              >
+                {code}
+              </code>
+            </pre>
+          </div>
         </div>
       </div>
       <div className="btn-mobile-view">
