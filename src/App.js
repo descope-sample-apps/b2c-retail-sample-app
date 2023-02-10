4ab31bc0617f4eb20460fe8a72358a5ab4655768
Gilad Shriki <shriki@descope.com>
2023-02-09 22:57:49 -0800

    remove comments

--- src/App.js
+++ src/App.js
@@ -22,12 +22,6 @@ const AppRoot = () => {
 }
 
 const App = () => {
-  // const { isAuthenticated } = useSession();
-  // // useEffect(() => {
-	// // 	if (isAuthenticated) {
-	// // 		me();
-	// // 	}
-	// // }, [isAuthenticated]);
   return (
     <div style={{height: '100%'}}>
       <Container/>
