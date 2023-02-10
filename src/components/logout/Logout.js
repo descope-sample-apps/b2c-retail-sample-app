be9e8da9ae24dc3ad0752fd4d2e27e8617068a84
Gilad Shriki <shriki@descope.com>
2023-02-09 22:44:23 -0800

    logout fix. flow code fix

--- src/components/logout/Logout.js
+++ src/components/logout/Logout.js
@@ -1,11 +1,12 @@
 import React from "react";
 import { useAuth } from "@descope/react-sdk";
+import { useNavigate } from "react-router";
 
 const Logout = () => {
   const { logout } = useAuth();
+  const navigate = useNavigate();
   logout();
-  window.location.href = "/";
-  return (<></>)
+  navigate("/")
 };
 
 export default Logout;
