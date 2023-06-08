e72f322a674f58b00ef02ffa47c3a5dc094753a4
Allen Zhou <46854522+allenzhou101@users.noreply.github.com>
2023-06-08 11:07:29 -0500

    Add unit test for App rendering

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,23 @@
+import React from 'react';
 import { render, screen } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('App', () => {
+  beforeEach(() => {
+    // Set up a mock for window.analytics
+    window.analytics = {
+      page: jest.fn(),
+    };
+  });
+
+
+  it('renders without crashing', () => {
+    render(
+      <MemoryRouter initialEntries={["/"]}>
+        <App />
+      </MemoryRouter>
+    );
+    expect(screen.getByTestId('container')).toBeInTheDocument();
+  });
 });
