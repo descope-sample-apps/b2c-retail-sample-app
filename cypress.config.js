730ed96cf855c44e62bbd0f9a1dfba475821c65f
Allen Zhou <46854522+allenzhou101@users.noreply.github.com>
2023-06-10 17:19:54 -0500

    Update cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -12,15 +12,9 @@ module.exports = defineConfig({
     },
   },
   env: {
-    // descope_username: process.env.DESCOPE_USERNAME,
-    // descope_password: process.env.DESCOPE_PASSWORD,
     descope_domain: process.env.REACT_APP_DESCOPE_DOMAIN,
-    // descope_audience: process.env.REACT_APP_DESCOPE_AUDIENCE,
-    // descope_scope: process.env.REACT_APP_DESCOPE_SCOPE,
     descope_project_id: process.env.REACT_APP_DESCOPE_PROJECT_ID,
     descope_management_id: process.env.REACT_APP_DESCOPE_MANAGEMENT_ID
-
-    // descope_client_secret: process.env.DESCOPE_CLIENT_SECRET,
   },
 });
 
