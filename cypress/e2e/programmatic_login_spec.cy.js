6fd5c040efee65bb2a988e753302dc0c401d03c3
Allen Zhou <46854522+allenzhou101@users.noreply.github.com>
2023-08-18 13:25:29 -0500

    Add logout test (#136)

--- cypress/e2e/programmatic_login_spec.cy.js
+++ cypress/e2e/programmatic_login_spec.cy.js
@@ -8,8 +8,8 @@ describe('Descope', function () {
     // // Now navigate to the root URL of your application.
     cy.visit('/')
 
-    // cy.get('.ant-modal-close-x > .anticon > svg').click(); // Close the modal
-    // cy.get('.main-nav .btntag > .nav-link').contains("Logout").click(); // Click the logout button
-    // cy.get('.main-nav .btntag > .nav-link').contains("Login").should('be.visible'); // Check that the login button is visible
+    cy.get('.ant-modal-close-x > .anticon > svg').click(); // Close the modal
+    cy.get('[data-cy="login-logout-button"]').click(); // Click the logout button
+    cy.get('[data-cy="login-logout-button"]').contains("Login"); // Check that the login button is visible
   })
 })
