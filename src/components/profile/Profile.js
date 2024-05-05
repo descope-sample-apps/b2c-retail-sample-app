bef0c2403daaeeb90a6ca3bbc40b2bb095092cf6
Reuven Zabirov <ruvenzx0@gmail.com>
2024-05-05 21:19:22 +0300

    logout

--- src/components/profile/Profile.js
+++ src/components/profile/Profile.js
@@ -6,7 +6,12 @@ function Profile() {
   return (
     <>
     <div>
-      {isAuthenticated || isSessionLoading ? <UserProfile widgetId="user-profile-widget"></UserProfile> : window.location.href = '/login'}
+      {isAuthenticated || isSessionLoading ? <UserProfile 
+      widgetId="user-profile-widget"     
+      onLogout={() => {
+        console.log('logging out')
+      window.location.href = '/login';
+    }}></UserProfile> : window.location.href = '/login'}
       </div>
     </>);
 
