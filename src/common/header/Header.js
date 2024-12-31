194a952efe17def86a3fe4fc29bcce0237f22752
Asaf Shen <asaf@descope.com>
2024-12-31 20:35:05 +0200

    Anonymous users (#235)
    * anonymus users
    
    * yarn

--- src/common/header/Header.js
+++ src/common/header/Header.js
@@ -1,129 +1,170 @@
 import React, { useEffect } from "react";
 import { useState } from "react";
-import { Link ,NavLink } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 import { useSession, useUser } from "@descope/react-sdk";
 import hamburger from "../../assets/hamburger.svg";
 import close from "../../assets/close.svg";
 import "./Header.css";
 import { Badge, Button } from "antd";
+import { jwtDecode } from "jwt-decode";
 
 const getDisplayName = (user) => {
-  return user?.name || user?.email || "";
+    return user?.name || user?.email || "";
 };
 
 function Header() {
-  let selectedItems = JSON.parse(localStorage.getItem("selectedItem"))
-    ? JSON.parse(localStorage.getItem("selectedItem"))
-    : [];
-  const [isNavExpanded, setIsNavExpanded] = useState(false);
-  const [cartLength, setCartLength] = useState([]);
-  const { isAuthenticated } = useSession();
-  const { user } = useUser();
-  const { loginLabel, linkPath } = isAuthenticated
-    ? { loginLabel: `${getDisplayName(user)} (Logout)`, linkPath: "/logout" }
-    : { loginLabel: "Login", linkPath: "/login" };
-  const doLogout = () => {
-    const localStorageKeysToRemove = [
-      "selectedItem",
-      "productData",
-      "newArrivalData",
-      "loginDetails",
-      "firstVisit",
-    ];
-    for (const key of localStorageKeysToRemove) {
-      localStorage.removeItem(key);
-    }
-    setIsNavExpanded(!isNavExpanded);
-  };
-  useEffect(() => {
-    if (cartLength && selectedItems.length !== cartLength.length)
-      setCartLength(selectedItems);
-  }, [selectedItems]);
+    let selectedItems = JSON.parse(localStorage.getItem("selectedItem"))
+        ? JSON.parse(localStorage.getItem("selectedItem"))
+        : [];
+    const [isNavExpanded, setIsNavExpanded] = useState(false);
+    const [cartLength, setCartLength] = useState([]);
+    const { isAuthenticated, sessionToken, isSessionLoading } = useSession();
+    const isAnonymousUser =
+        (sessionToken && jwtDecode(sessionToken)["danu"]) || false;
+    const isLoggedIn = isAuthenticated && !isAnonymousUser;
 
-  return (
-    <>
-      <nav className="main-nav">
-        <button
-          className="hamburger"
-          onClick={() => {
-            setIsNavExpanded(!isNavExpanded);
-          }}
-        >
-          {!isNavExpanded ? (
-            <img src={hamburger} alt="hamburger" />
-          ) : (
-            <img src={close} alt="close" />
-          )}
-        </button>
-        <div className="logo">
-          <Link to="/">
-            <h2>Tee-Hee Tees</h2>
-          </Link>
-        </div>
+    const { user } = useUser();
+    const { loginLabel, linkPath } = isLoggedIn
+        ? {
+              loginLabel: `${getDisplayName(user)} (Logout)`,
+              linkPath: "/logout",
+          }
+        : { loginLabel: "Login", linkPath: "/login" };
+    const doLogout = () => {
+        const localStorageKeysToRemove = [
+            "selectedItem",
+            "productData",
+            "newArrivalData",
+            "loginDetails",
+            "firstVisit",
+        ];
+        for (const key of localStorageKeysToRemove) {
+            localStorage.removeItem(key);
+        }
+        setIsNavExpanded(!isNavExpanded);
+    };
+    useEffect(() => {
+        if (cartLength && selectedItems.length !== cartLength.length)
+            setCartLength(selectedItems);
+    }, [selectedItems]);
 
-        <div className={!isNavExpanded ? "navbar-expanded" : "navbar"}>
-          <ul className="menu-list">
-            <li className="menu-lists">
-              <NavLink
-                to="/"
-                className="menu-title"
-                onClick={() => {
-                  setIsNavExpanded(!isNavExpanded);
-                }}
-              >
-                Popular
-              </NavLink>
-            </li>
-            <li className="menu-lists">
-              <NavLink
-                to="/"
-                className="menu-title"
-                onClick={() => {
-                  setIsNavExpanded(!isNavExpanded);
-                }}
-              >
-                New
-              </NavLink>
-            </li>
-            <li className="menu-lists">
-              <div className="btntag-mobile">
-                <NavLink to={linkPath} className="nav-link" onClick={() => isAuthenticated ? doLogout() : setIsNavExpanded(!isNavExpanded)}>
-                  <Button className={isAuthenticated ? "btn-login" : "login-nav"}>
-                    {loginLabel}
-                  </Button>
-                </NavLink>
-              </div>
-            </li>
-          </ul>
-        </div>
-        <div className="navbar-right">
-          <div className="btntag">
-            <NavLink data-cy="login-logout-button" to={linkPath} className="nav-link" onClick={() => isAuthenticated ? doLogout() : ''}>
-              {loginLabel}
-            </NavLink>
-          </div>
-          <NavLink to="cart">
-            <Badge
-              count={cartLength.length}
-              overflowCount={100}
-              style={{ top: "2em" }}
-              className="cart-display"
-            >
-              <img src={require("../../assets/cart.svg").default} alt="cart-img" />
-            </Badge>
-          </NavLink>
-          <NavLink to="profile">
-            <Badge
-              style={{ top: "2em" }}
-              className="profile-display"
-            >
-              <img src={require("../../assets/profile.svg").default} alt="profile-img" />
-            </Badge>
-          </NavLink>
-        </div>
-      </nav>
-    </>
-  );
+    return (
+        <>
+            <nav className="main-nav">
+                <button
+                    className="hamburger"
+                    onClick={() => {
+                        setIsNavExpanded(!isNavExpanded);
+                    }}
+                >
+                    {!isNavExpanded ? (
+                        <img src={hamburger} alt="hamburger" />
+                    ) : (
+                        <img src={close} alt="close" />
+                    )}
+                </button>
+                <div className="logo">
+                    <Link to="/">
+                        <h2>Tee-Hee Tees</h2>
+                    </Link>
+                </div>
+
+                <div className={!isNavExpanded ? "navbar-expanded" : "navbar"}>
+                    <ul className="menu-list">
+                        <li className="menu-lists">
+                            <NavLink
+                                to="/"
+                                className="menu-title"
+                                onClick={() => {
+                                    setIsNavExpanded(!isNavExpanded);
+                                }}
+                            >
+                                Popular
+                            </NavLink>
+                        </li>
+                        <li className="menu-lists">
+                            <NavLink
+                                to="/"
+                                className="menu-title"
+                                onClick={() => {
+                                    setIsNavExpanded(!isNavExpanded);
+                                }}
+                            >
+                                New
+                            </NavLink>
+                        </li>
+                        <li className="menu-lists">
+                            <div className="btntag-mobile">
+                                {isSessionLoading ? (
+                                    <div className="loading">Loading...</div>
+                                ) : (
+                                    <NavLink
+                                        to={linkPath}
+                                        className="nav-link"
+                                        onClick={() =>
+                                            isLoggedIn
+                                                ? doLogout()
+                                                : setIsNavExpanded(
+                                                      !isNavExpanded
+                                                  )
+                                        }
+                                    >
+                                        <Button
+                                            className={
+                                                isLoggedIn
+                                                    ? "btn-login"
+                                                    : "login-nav"
+                                            }
+                                        >
+                                            {loginLabel}
+                                        </Button>
+                                    </NavLink>
+                                )}
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+                <div className="navbar-right">
+                    <div className="btntag">
+                        <NavLink
+                            data-cy="login-logout-button"
+                            to={linkPath}
+                            className="nav-link"
+                            onClick={() => (isLoggedIn ? doLogout() : "")}
+                        >
+                            {loginLabel}
+                        </NavLink>
+                    </div>
+                    <NavLink to="cart">
+                        <Badge
+                            count={cartLength.length}
+                            overflowCount={100}
+                            style={{ top: "2em" }}
+                            className="cart-display"
+                        >
+                            <img
+                                src={require("../../assets/cart.svg").default}
+                                alt="cart-img"
+                            />
+                        </Badge>
+                    </NavLink>
+                    <NavLink to="profile">
+                        <Badge
+                            style={{ top: "2em" }}
+                            className="profile-display"
+                        >
+                            <img
+                                src={
+                                    require("../../assets/profile.svg").default
+                                }
+                                alt="profile-img"
+                            />
+                        </Badge>
+                    </NavLink>
+                </div>
+            </nav>
+        </>
+    );
 }
 
 export default Header;
--- src/components/profile/Profile.js
+++ src/components/profile/Profile.js
@@ -1,20 +1,33 @@
 import { UserProfile, useSession } from "@descope/react-sdk";
-import './profile.css';
-
+import "./profile.css";
+import { jwtDecode } from "jwt-decode";
+import { useEffect } from "react";
 function Profile() {
-  const { isAuthenticated, isSessionLoading } = useSession();
+    const { isAuthenticated, sessionToken, isSessionLoading } = useSession();
+    const isAnonymousUser =
+        (sessionToken && jwtDecode(sessionToken)["danu"]) || false;
+    const isLoggedIn = isAuthenticated && !isAnonymousUser;
 
-  return (
-    <>
-    <div className="profile">
-      {isAuthenticated || isSessionLoading ? <UserProfile 
-      widgetId="user-profile-widget"     
-      onLogout={() => {
-      window.location.href = '/login';
-    }}></UserProfile> : window.location.href = '/login'}
-      </div>
-    </>);
-
+    useEffect(() => {
+        if (!isSessionLoading && !isLoggedIn) {
+            window.location.href = "/login";
+        }
+    }, [isSessionLoading, isLoggedIn]);
+    return (
+        <>
+            <div className="profile">
+                {isSessionLoading && <div className="loading">Loading...</div>}
+                {!isSessionLoading && isLoggedIn && (
+                    <UserProfile
+                        widgetId="user-profile-widget"
+                        onLogout={() => {
+                            window.location.href = "/login";
+                        }}
+                    />
+                )}
+            </div>
+        </>
+    );
 }
 
 export default Profile;
